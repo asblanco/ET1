95061e473db641cec9285a5c9f45c977bb58e7a1
Brais Cao González <Brais@iMac-de-Brais.local>
2015-10-15 23:03:34 +0200

    Cambios en Login.php, Registro.php y form.css

--- php/Registro.php
+++ php/Registro.php
@@ -7,14 +7,28 @@ Formulario de registro. Envia la informacion por POST a ProcesarRegistro.php
 ============================================================================
 -->
 
-<form action='ProcesarRegistro.php' method='POST'>
-
-Login : <input type='text' name='login'><BR>
-Password : <input type='password' name='pass'><BR>
-Nombre : <input type='text' name='nombre'><BR>
-Apellidos : <input type='text' name='apellidos'><BR>
-Email : <input type='text' name='email'><BR>
-
-<input type='submit' name='accion' value='validar'>
-
-</form>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/> 
+	<link rel="stylesheet" type="text/css" href="../css/form.css">
+</head>
+<body>
+	<section class="jumbotron">
+		<div class="container" align="center">
+			<form action='ProcesarRegistro.php' method='POST'>
+				<label for="nombre">Usuario</label>
+				<input type='text' name='login'><BR>
+				<label for="apellidos">Contraseña</label>
+				<input type='password' name='pass'><BR>
+				<label for="apellidos">Nombre</label>
+				<input type='text' name='nombre'><BR>
+				<label for="apellidos">Apellidos</label>
+				<input type='text' name='apellidos'><BR>
+				<label for="apellidos">Email</label>
+				<input type='text' name='email'><BR>
+				<input type='submit' name='accion' value='validar'>
+			</div>
+		</section>		
+	</form>
+</body>
+<html>	

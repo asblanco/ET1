9681f08c5b5497c33979cc50cdc7a49cf9ca777d
edconde <edconde14@gmail.com>
2015-10-29 17:39:21 +0100

    Controladores básicos

--- controladores/ctrl_usu.php
+++ controladores/ctrl_usu.php
@@ -1,3 +1,23 @@
-<?php
+<!--
+===========================================================================
+Controlador para mostrar los datos de los usuarios
+Creado por: Edgar Conde Nóvoa
+Fecha: 29/10/2015
+============================================================================
+-->
 
+<?php
+    include_once "../modelo/model_usu.php";
+
+    //Conectar con la bd
+    $db = new Database();
+
+    //Conectar con el modelo de Funcionalidad
+    $usuarios = new Usuario();
+    
+    //Array asociativo de la tabla Usuario, tiene Login, Nombre, etc..
+    $arrayUsuarios = $usuarios->listar();
+
+    //Desconectar de la base de datos
+    $db->desconectar();
 ?>

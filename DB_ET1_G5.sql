1c64f751d7db39cebd036b14b1afd54011d983ab
Andrea <andreasanchezblanco@gmail.com>
2015-10-30 01:23:02 +0100

    BD
    Cuidado con generar la BD desde phpmyadmin, se habia eliminado la
    creación automatica de la BD y alguna cosa más

--- DB_ET1_G5.sql
+++ DB_ET1_G5.sql
@@ -1,9 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.2.12deb2
--- http://www.phpmyadmin.net
---
 -- Servidor: localhost
--- Tiempo de generación: 30-10-2015 a las 00:39:56
+-- Tiempo de generación: 20-10-2015 a las 17:56:33
 -- Versión del servidor: 5.5.44-0+deb8u1
 -- Versión de PHP: 5.6.13-0+deb8u1
 
@@ -19,6 +15,14 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `DB_ET1_G5`
 --
+DROP DATABASE IF EXISTS DB_ET1_G5;
+CREATE DATABASE DB_ET1_G5 default character set utf8 default collate utf8_spanish_ci;
+grant usage on *.* to 'admin'@'localhost';
+drop user 'admin'@'localhost';
+create user 'admin'@'localhost' identified by 'iu';
+grant all on DB_ET1_G5.* to 'admin'@'localhost';
+
+USE DB_ET1_G5;
 
 -- --------------------------------------------------------
 
@@ -36,10 +40,10 @@ CREATE TABLE IF NOT EXISTS `Funcionalidad` (
 --
 
 INSERT INTO `Funcionalidad` (`NombreFun`, `DescFun`) VALUES
-('Consultar Usuario', 'Permite consultar usuarios de la aplicación.'),
 ('Crear Usuario', 'Permite crear usuarios de la aplicación.'),
-('Eliminar Usuario', 'Permite eliminar usuarios de la aplicación.'),
-('Modificar Usuario', 'Permite modificar usuarios de la aplicación.');
+('Consultar Usuario', 'Permite consultar usuarios de la aplicación.'),
+('Modificar Usuario', 'Permite modificar usuarios de la aplicación.'),
+('Eliminar Usuario', 'Permite eliminar usuarios de la aplicación.');
 
 --
 -- Disparadores `Funcionalidad`
@@ -70,7 +74,6 @@ CREATE TRIGGER `after_insert_pagina` AFTER INSERT ON `Pagina`
  FOR EACH ROW INSERT INTO Usu_Pag (Login, Url) VALUES ('admin', NEW.Url)
 //
 DELIMITER ;
-
 -- --------------------------------------------------------
 
 --
@@ -79,7 +82,7 @@ DELIMITER ;
 
 CREATE TABLE IF NOT EXISTS `Rol` (
   `NombreRol` varchar(65) NOT NULL,
-  `DescRol` varchar(65) DEFAULT NULL
+  `DescRol` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -87,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `Rol` (
 --
 
 INSERT INTO `Rol` (`NombreRol`, `DescRol`) VALUES
-('Administrador', ' El administrador debe poder modificar todo. Teniendo todas las f');
+('Administrador', ' El administrador debe poder modificar todo. Teniendo todas las funcionalidades asignadas.');
 
 -- --------------------------------------------------------
 
@@ -105,10 +108,11 @@ CREATE TABLE IF NOT EXISTS `Rol_Fun` (
 --
 
 INSERT INTO `Rol_Fun` (`NombreRol`, `NombreFun`) VALUES
-('Administrador', 'Consultar Usuario'),
 ('Administrador', 'Crear Usuario'),
-('Administrador', 'Eliminar Usuario'),
-('Administrador', 'Modificar Usuario');
+('Administrador', 'Modificar Usuario'),
+('Administrador', 'Consultar Usuario'),
+('Administrador', 'Eliminar Usuario') ;
+
 
 -- --------------------------------------------------------
 
@@ -119,25 +123,20 @@ INSERT INTO `Rol_Fun` (`NombreRol`, `NombreFun`) VALUES
 CREATE TABLE IF NOT EXISTS `Usuario` (
   `Login` varchar(65) NOT NULL,
   `Password` varchar(65) NOT NULL,
-  `Idioma` enum('es','en') CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `Nombre` varchar(65) NOT NULL,
   `Apellidos` varchar(65) NOT NULL,
   `Email` varchar(65) NOT NULL,
-  `FechaAlta` date NOT NULL
+  `FechaAlta` date NOT NULL,
+  `Idioma` enum('es','en') CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `Usuario`
 --
 
-<<<<<<< HEAD
-INSERT INTO `Usuario` (`Login`, `Password`, `Idioma`, `Nombre`, `Apellidos`, `Email`, `FechaAlta`) VALUES
-('admin', 'admin', 'es', 'admin', 'admin', 'admin', '2015-10-14');
-=======
 INSERT INTO `Usuario` (`Login`, `Password`, `Nombre`, `Apellidos`, `Email`, `FechaAlta`) VALUES
 ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', 'admin', '2015-10-14');
 -- password admin
->>>>>>> origin/master
 
 -- --------------------------------------------------------
 
@@ -147,7 +146,7 @@ INSERT INTO `Usuario` (`Login`, `Password`, `Nombre`, `Apellidos`, `Email`, `Fec
 
 CREATE TABLE IF NOT EXISTS `Usu_Pag` (
   `Login` varchar(65) NOT NULL,
-  `Url` varchar(65) NOT NULL
+  `Url` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -244,6 +243,7 @@ ADD CONSTRAINT `FK_Pagina` FOREIGN KEY (`Url`) REFERENCES `Pagina` (`Url`) ON DE
 ALTER TABLE `Usu_Rol`
 ADD CONSTRAINT `FK_Rol` FOREIGN KEY (`NombreRol`) REFERENCES `Rol` (`NombreRol`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `FK_Usuario` FOREIGN KEY (`Login`) REFERENCES `Usuario` (`Login`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

365c93d6f697f370cf2f2c665c935480e70711e9
edconde <edconde14@gmail.com>
2015-11-04 11:57:10 +0100

    Cambios en Usu para que muestre sus Paginas

--- vistas/vista_usu.php
+++ vistas/vista_usu.php
@@ -102,7 +102,7 @@ include_once('../controladores/ctrl_usu.php');
                 // array asociativo de las paginas ligadas al usuario actual del bucle
                 $arrayPaginas = $usuX['paginas'];
                 foreach ($arrayPaginas as $pag ){
-                    echo "<p> {$pag['Url']} </p>";
+                    echo "<p> {$pag['NombrePag']} </p>";
                 }
                  ?>   
             </div>

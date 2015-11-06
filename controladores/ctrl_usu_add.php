572156382b1ba44d9eb120cee49d27a1dfa7c1ef
Eliasmb7 <eliasmb7@gmail.com>
2015-11-06 12:38:41 +0100

    cambio ctrl_usu_add

--- controladores/ctrl_usu_add.php
+++ controladores/ctrl_usu_add.php
@@ -7,6 +7,7 @@ Fecha: 28/10/2015
 -->
 
 <?php
+    include '../modelo/connect_DB.php';
     include_once "../modelo/model_usu.php";
     
     $db = new Database();
@@ -17,7 +18,37 @@ Fecha: 28/10/2015
 
     //Se debe pasar el nombre, roles y hacer un bucle para añadir los roles y paginas asociadas
         
-    
+		
+		
+		
+	//Recogemos las variables
+		$login=$_POST['login'];
+		$nombre=$_POST['nombre'];
+		$apellidos= $_POST['apellidos'];
+		$fechaAlta="";
+		$email= $_POST['email'];
+		$password= $_POST['password'];
+		$roles= $_POST['roles'];
+		$paginas= $_POST['paginas'];
+		$idioma="es";
+
+  //Comprobamos si ya existe el usuario
+    $consultaSiUsu = $newUsu->exists($login);
+    if ($consultaSiUsu == true){
+        echo '<p>El usuario ' . $login . ' ya existe en la bd</p>';
+    } else {
+        $insertUsu = new Usuario ($login,$nombre,$apellidos,$fechaAlta,$email,$password,$idioma,$roles,$paginas);
+        if ($newUsu->crear($insertUsu) == true){
+            echo 'El usuario ' . $login. ' ha sido registrado en el sistema';
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+ 
+        } else{
+            echo "Error al insertar el usuario";
+        }
+    }
+
+
+
 
     $db->desconectar();
 ?>

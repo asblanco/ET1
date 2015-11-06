36320c0e64ca4f72a8709ca4d98a87a9131fbb70
ansich <dafernandez2@esei.uvigo.es>
2015-11-06 17:14:02 +0100

    Update vista_func_mod.php

--- vistas/vista_func_mod.php
+++ vistas/vista_func_mod.php
@@ -1,8 +1,8 @@
 <!--
 ===========================================================================
 Modifica un rol
-Creado por: Lucas Rodicio Conde
-Fecha: 02/11/2015
+Creado por: Andrea Sanchez Blanco, Edgar Conde Novoa
+Fecha: 25/10/2015
 ============================================================================
 -->
 <?php
@@ -22,13 +22,11 @@ Fecha: 02/11/2015
     include('../html/navBar.html'); 
     //Para poder visualizar los datos
     include_once('../controladores/ctrl_func.php');
-    //Obtiene el nombre de la funcionalidad a modificar de la URL
+    //Obtiene el nombre de la func a modificar de la URL
     $funcName = $_GET['func'];
-
-    //Obtiene los datos de la funcionalidad en un array asociativo
+    //Obtiene los datos del rol en un array asociativo
     $f = new Funcionalidad();
     $func = $f->consultar($funcName); 
-
 ?>
 
 <html lang="en">
@@ -41,23 +39,23 @@ Fecha: 02/11/2015
               <div class="panel-heading"><?php echo $idioma["modificar_funcionalidad_func"]; ?></div>
               <div class="panel-body">
                 <div class="form-group">
-                    <label for="func"><?php echo $idioma["modificar_funcionalidad_nombre"]; ?></label>
-                    <input type="text" class="form-control" name="funcionalidad" value="<?php echo $funcName; ?>">
-                    <!-- Campo oculto para pasar el nombre de la funcionalidad al ctrl de modificar -->
+                    <label for="rol"><?php echo $idioma["modificar_funcionalidad_nombre"]; ?></label>
+                    <input type="text" class="form-control" name="newName" value="<?php echo $funcName; ?>">
+                    <!-- Campo oculto para pasar el nombre de la func al ctrl de modificar -->
                     <input hidden="hidden" type="text" name="oldName" value="<?php echo $funcName; ?>">
                 </div>
                   
                 <div class="form-group">
                     <label for="comment"><?php echo $idioma["modificar_funcionalidad_descripcion"]; ?></label>
-                    <textarea class="form-control" rows="5" name="comment"><?php echo $func['descripcion'] ?></textarea>
+                    <textarea class="form-control" rows="5" name="newDesc"><?php echo $func['descripcion'] ?></textarea>
                 </div>
               </div>
             </div>
             
-            <!-- Lista de paginas asociados a la funcionalidad -->
+            <!-- Lista de roles asociados a la func -->
             <div class="panel panel-default">
               <div class="panel-heading">
-              <?php echo $idioma["modificar_funcionalidad_paginas"]; ?>
+              <?php echo $idioma["modificar_funcionalidad_roles"]; ?>
                     <div class="pull-right">
                       <div class="dropdown">
                         <a href="#" data-toggle="dropdown">
@@ -65,8 +63,8 @@ Fecha: 02/11/2015
                           <!-- Contenido del dropdown -->
                           <ul class="dropdown-menu">
                               <?php 
-                              foreach($paginas as $pag){ ?>
-                                  <li><a href="#" class="small" data-value="<?php echo $pag['Url']; ?>" tabIndex="-1"><input type="checkbox"/>&nbsp; <?php echo $pag['Url']; ?> </a></li>
+                              foreach($roles as $r){ ?>
+                                  <li><a href="#" class="small addRol valor" value="<?php echo $r['NombreRol']; ?>" tabIndex="-1"><input type="checkbox"/>&nbsp; <?php echo $r['NombreRol']; ?> </a></li>
                               <?php
                               }
                               ?>
@@ -76,36 +74,49 @@ Fecha: 02/11/2015
                   </div>
                </div>
               <!-- List group -->
-              <ul class="list-group list-onHover">
-                <?php 
-                  foreach ($func['paginas'] as $pag){ ?>
+              <ul class="list-group list-onHover addRol">
+                <?php
+                  foreach ($func['roles'] as $rol){ ?> 
                     <li class="list-group-item">
-                        <?php echo $pag['Url'] ?>
-                        <a class="rm" href="#" onclick="removePag()"><div class="glyphicon glyphicon-trash"></div></a>
-                    <!-- Elemento oculto para pasar el array con las paginas modificadas por POST -->
-                    <input hidden="hidden" type="text" name="newPag[]" value="<?php echo $pag['Url']; ?>">
+                        <?php echo $rol['NombreRol'] ?>
+                        <a class="rm" href="#" onclick="removeRol()"><div class="glyphicon glyphicon-trash"></div></a>
+                    <!-- Elemento oculto para pasar el array con los roles modificados por POST -->
+                    <input hidden="hidden" type="text" name="newRolFunc[]" value="<?php echo $rol['NombreRol']; ?>">
                     </li>
                 <?php } ?>
               </ul>
             </div>
             
-            <!-- Lista de roles asociados a la funcionalidad -->
+            <!-- Lista de paginas asociadas a la func -->
             <div class="panel panel-default">
               <div class="panel-heading">
-              <?php echo $idioma["modificar_funcionalidad_roles"]; ?>
+              <?php echo $idioma["modificar_funcionalidad_pagina"]; ?>
                   <div class="pull-right">
-                    <a href="#"><div class="glyphicon glyphicon-plus"></div></a>
+                    <div class="dropdown">
+                        <a href="#" data-toggle="dropdown">
+                          <div class="glyphicon glyphicon-plus dropdown-toggle"></div>
+                          <!-- Contenido del dropdown -->
+                          <ul class="dropdown-menu">
+                              <?php 
+                              foreach($paginas as $p){ ?>
+                                  <li><a href="#" class="small addPag" value="<?php echo $p['NombrePag']; ?>" tabIndex="-1"><input type="checkbox"/>&nbsp; <?php echo $p['NombrePag']; ?> </a></li>
+                              <?php
+                              }
+                              ?>
+                          </ul>
+                        </a>
+                    </div>
                   </div>
                 </div>
                 <!-- List group -->
-                <ul class="list-group list-onHover">
+                <ul class="list-group list-onHover addP">
                     <?php 
-                      foreach ($func['roles'] as $rol){ ?>
+                      foreach ($func['paginas'] as $pag){ ?>
                         <li class="list-group-item">
-                            <?php echo $rol['NombreRol'] ?>
-                            <a href="#" class="rm" onclick="removeRol()"><div class="glyphicon glyphicon-trash"></div></a>
-                        <!-- Elemento oculto para pasar el array con los roles modificados por POST -->
-                            <input hidden="hidden" type="text" name="newRol[]" value="<?php echo $rol['NombreRol']; ?>">
+                            <?php echo $pag['NombrePag'] ?>
+                            <a href="#" class="rm" onclick="removePag()"><div class="glyphicon glyphicon-trash"></div></a>
+                        <!-- Elemento oculto para pasar el array con las paginas modificados por POST -->
+                            <input hidden="hidden" type="text" name="newPagFunc[]" value="<?php echo $pag['NombrePag']; ?>">
                         </li>
                         
                     <?php } ?>
@@ -115,7 +126,7 @@ Fecha: 02/11/2015
             <!-- Boton guardar -->
             <div class="btn-parent">
                 <div class="btn-child"> <!-- centran el boton -->
-                    <button type="submit" class="btn btn-info btn-lg" value=<?php echo $idioma["modificar_funcionalidad_guardar"]; ?>><?php echo                                          $idioma["reg_guardar"]; ?>
+                    <button type="submit" class="btn btn-info btn-lg" value=<?php echo $idioma["modificar_funcionalidad_guardar"]; ?>><?php echo $idioma["reg_guardar"]; ?>
                     <div class="glyphicon glyphicon-save"></div>
                     </button>
                 </div>
@@ -124,12 +135,12 @@ Fecha: 02/11/2015
       </form>
         
     <script>
-        function removePag() {
+        function removeRol() {
             $('.rm').click(function(){
               $(this).parents('li').remove();
             })
         }
-        function removeRol() {
+        function removePag() {
             $('.rm').click(function(){
               $(this).parents('li').remove();
             })
@@ -142,3 +153,17 @@ Fecha: 02/11/2015
 
 <!--Importar los jquery, bootstrap.js y el footer-->
 <?php include('../html/footer.html'); ?>
+
+<script>
+$(document).ready(function(){
+    $(".addRol").click(function(){
+        var value = $(this).attr("value");
+        $(".addR").append(" <li class='list-group-item'>"+ value +" <a class='rm' href='#' onclick='removeRol()'><div class='glyphicon glyphicon-trash'></div></a><input hidden='hidden' type='text' name='newRolFunc[]' value='"+ value +"'></li>");
+    });
+
+    $(".addPag").click(function(){
+        var value = $(this).attr("value");
+        $(".addP").append(" <li class='list-group-item'>"+ value +" <a class='rm' href='#' onclick='removePag()'><div class='glyphicon glyphicon-trash'></div></a><input hidden='hidden' type='text' name='newPagFunc[]' value='"+ value +"'></li>");
+    });
+});
+</script>

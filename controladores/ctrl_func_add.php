e47f4668a2bd96a2eac80309345691b4ce2984ea
ansich <dafernandez2@esei.uvigo.es>
2015-11-06 13:30:06 +0100

    Update ctrl_func_add.php

--- controladores/ctrl_func_add.php
+++ controladores/ctrl_func_add.php
@@ -1,19 +1,46 @@
 <!--
 ===========================================================================
-Controlador para añadir funcionalidades a la base de datos
-Creado por: David Ansia Fernández
-Fecha: 31/10/2015
+Controlador para procesar un nuevo registro
+Creado por: David Ansia
+Fecha: 01/11/2015
 ============================================================================
 -->
 
 <?php
-    include_once "../modelo/model_func.php";
-    
-    $funName ; //traer de la vista
-    $funDesc;
+    include '../modelo/connect_DB.php';
+    include '../modelo/model_func.php';
 
-    $func = new Funcionalidad($funName);
+	//Recogemos variables
+    $nombre= $_POST['nombre'];
+    $paginas= $_POST['Paginas'];
+    $roles= $_POST['Roles'];
+    $desc=$_POST['Roles'];
+	
 
-    //Se debe pasar el nombre, descripcion, usuarios y hacer un bucle para añadir los usuarios y funcionalidades asociadas
+   //Conectamos con el gestor de la bd
+    $db = new Database();
+    $newFunc = new Funcionalidad();
+
+    //Comprobamos si ya existe la funcionalidad
+    $consultaSiFunc = $newFunc->exists($nombre);
+    if ($consultaSiFunc == true){
+        echo '<p>La funcionalidad ' . $nombre . ' ya existe en la bd</p>';
+    } else {
+        $insertFunc = new Funcionalidad ($nombre, $desc, $roles, $paginas);
+        echo 'bien';
+        if ($newFunc->crear($insertFunc) == true){
+            echo 'La funcionalidad ' . $nombre . ' ha sido registrada en el sistema';
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+ 
+        } else{
+            echo "Error al insertar la funcionalidad";
+        }
+    }
 
 ?>
+
+<!--
+<script type="text/javascript">
+setTimeout("window.location = '<?php echo $_SERVER['HTTP_REFERER'] ?>'", 4000);
+</script>
+-->

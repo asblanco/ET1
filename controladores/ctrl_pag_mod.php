e77a13d6d5b380601cdb52bfa8903e7354d83dac
JorgeRodicio <jorgerodicio@gmail.com>
2015-11-06 20:04:00 +0100

    Update ctrl_pag_mod.php

--- controladores/ctrl_pag_mod.php
+++ controladores/ctrl_pag_mod.php
@@ -1,14 +1,15 @@
 <?php
     include_once "../modelo/model_pag.php";
 
-    $modFunc = new Funcionalidad();
-
+    $modPag = new Pagina();
+    $db = new Database();
     //Nuevos datos
-    $oldPagName = $_POST['oldName'];
-    $newPagName = $_POST['pag'];
-    $newFuncDesc = $_POST['comment'];
+    $oldPagName = $_POST['oldName'];  
+    $newPagDesc = $_POST['comment'];
     $usu = array();
-    $func = array();
+    $func = $_POST['newFunc'];
+    $url = $oldPagName;
+   
 
     if(isset($_POST['newUsu'])){
       if (is_array($_POST['newUsu'])) {
@@ -17,20 +18,13 @@
         }
       }
     }
-
-    if(isset($_POST['newFunc'])){
-      if (is_array($_POST['newFunc'])) {
-        foreach($_POST['newFunc'] as $value){
-          $func[] = $value;
-        }
-      }
-    }
-
-    $newPag = new Pag($newPagName, $newPagDesc, $usu, $func);
-    if ($modFunc->modificar($oldFuncName, $newFunc) == true){
+  
+    $newPag = new Pagina($url, $oldPagName, $newPagDesc, $usu, $func);
+    if ($modPag->modificar($url, $newPag) == true){
         header('location:../vistas/vista_pag.php'); 
     }else {
         echo "Fallo en la actualizacion de la pagina";
     }
 
 ?>
+

c93d44cdc2377248acb31a66252de199b55d8859
ansich <dafernandez2@esei.uvigo.es>
2015-11-09 16:53:12 +0100

    Update ctrl_func_mod.php

--- controladores/ctrl_func_mod.php
+++ controladores/ctrl_func_mod.php
@@ -15,13 +15,10 @@ Fecha: 06/11/2015
     $oldFuncName = $_POST['oldName'];
     $newFuncName = $_POST['newName'];
     $newFuncDesc = $_POST['newDesc'];
-    $newRoles = array();
-    $newPags = array();
 
-    $newFunc = new Funcionalidad($newFuncName, $newFuncDesc, $newRoles, $newPags);
-    if ($modFunc->modificar($oldFuncName, $newFunc) == true){
+    $newFunc = new Funcionalidad($newFuncName, $newFuncDesc);
+    
+    
         header('location:../vistas/vista_func.php'); 
-    }else {
-        echo "Fallo en la actualizacion de la funcionalidad";
-    }
 ?>
+

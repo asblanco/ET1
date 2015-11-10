981a4cdb24285d25eed2b81c9a8e0141b946701f
developergate <edgard.rglz@gmail.com>
2015-11-10 04:37:23 +0100

    Script comprobacion tipo usuario
    Si eres Administrador vas al menu de nuestra app si no al de rodeiro
    pero siempre comprobando que el login y la pass funcionen

--- controladores/ctrl_procesar_login.php
+++ controladores/ctrl_procesar_login.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+
+<?php
 //========================================================================================
 //Recoge login y password del formulario de login Login.php. Accede a la bd y comprueba si
 //el login existe, si no existe informa. Despues comprueba si la password coincide, si no coincide informa.
@@ -19,15 +24,36 @@ $db = new Database();
 $ExisteLogin = 'SELECT * FROM Usuario WHERE Login = \''. $login . '\'';
 $ResultadoExisteLogin = $db->consulta($ExisteLogin) or die('No se puede comprobar si existe login');
 
-if (mysqli_num_rows($ResultadoExisteLogin)==1)
-{
+	if (mysqli_num_rows($ResultadoExisteLogin)==1)
+	{
 	//si existe el login
 	//sacamos la fila de usuario del recordset
 	$TuplaLogin = mysqli_fetch_array($ResultadoExisteLogin);
 	//comprobamos si el atributo PASSWORD coincide con lo introducido por el usuario como password para ese login
 	//echo $pass;
-	if ($TuplaLogin['Password'] == $pass)
-	{
+
+	$consultaTipoUsuario = 'SELECT NombreRol FROM Usu_Rol WHERE Login = \''. $login . '\'';
+	$resultadoTipo = $db->consulta($consultaTipoUsuario);
+	$tuplatipo = mysqli_fetch_array($resultadoTipo);
+	
+		if ($tuplatipo['NombreRol'] == 'Administrador' && $TuplaLogin['Password'] == $pass )
+		{	
+		session_start();
+		$_SESSION["login_usuario"] = $login;
+
+		$consultaIdioma = 'SELECT idioma FROM Usuario WHERE Login = "'.$login.'"';
+		$resultado = $db->consulta($consultaIdioma);
+		$tuplaIdioma = mysqli_fetch_array($resultado);
+
+		$_SESSION["idioma_usuario"] = $tuplaIdioma["idioma"];
+		header('Location:../vistas/menu.php');
+
+		}
+
+
+
+		else if ($TuplaLogin['Password'] == $pass)
+		{
 		session_start();
 		$_SESSION["login_usuario"] = $login;
 
@@ -38,20 +64,22 @@ if (mysqli_num_rows($ResultadoExisteLogin)==1)
 
 		$_SESSION["idioma_usuario"] = $tuplaIdioma["idioma"];
 
-		header('Location:../vistas/menu.php');
-	}
+		header('Location:../paginas/C_Menu.php');
+		}
+	
 	else
 	//la pass introducida por el usuario no es correcta para ese login
 	{
 		echo 'Error al introducir la password para ese login';
 
-	}	
-}
+	}
+}	
+
 //si es incorrecta
-else
-{
+	else
+	{
 	echo 'Error, no existe ese login';
 
-}
+	}
 
 ?>
